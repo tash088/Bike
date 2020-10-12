6c1b55212f19e6b6fbb515a6dee6b8f191410c20
Samantha Ashby <samanthaashby@Samanthas-MacBook-Pro.local>
2020-10-12 07:30:54 -0400

    Working file with most of core components working properly

---
+++ Project_2_Working.R
@@ -0,0 +1,81 @@
+#Install packages
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(caret)
+library(GGally)
+library(scales)
+library(corrplot)
+
+#Read in bike share data
+bike<-read_csv("day.csv")
+
+#Drop casual and registered variables, then filter to desired weekday
+bike<-select(bike,-casual,-registered) %>% filter(weekday==1)
+view(bike)
+
+#Partition into training and test sets
+set.seed(123)
+bikeIndex<-createDataPartition(bike$cnt,p=0.7,list=FALSE)
+bikeTrain<-bike[bikeIndex,]
+bikeTest<-bike[bikeIndex,]
+
+#View summary statistics for training data
+g<-ggplot(bikeTrain,aes(x=cnt,y=..density..))
+g+geom_histogram()+labs(title="Histogram of Bike Counts")#how to fix y-axis %s
+summary(bikeTrain,digits=2)
+
+#Create scatterplots for variables that are likely to be important predictors
+g<-ggplot(bikeTrain,aes(y=cnt,fill=as.factor(workingday)))
+
+g+geom_boxplot()+ylab("Bike Count")+xlab("Is it a Working Day?")+ 
+  labs(title="Relationship of workingday to cnt") #why is x-axis -0.2 to 0.2?
+
+
+g<-ggplot(bikeTrain,aes(y=cnt,fill=as.factor(weathersit)))
+
+g+geom_boxplot()+ylab("Bike Count")+xlab("What is the weather like")+ 
+  labs(title="Relationship of weathersit to cnt") #why is x-axis -0.2 to 0.2?
+
+
+g<-ggplot(bikeTrain,aes(y=cnt,fill=as.factor(season)))
+
+g+geom_boxplot()+ylab("Bike Count")+xlab("What season is it?")+ 
+  labs(title="Relationship of season to cnt") #why is x-axis -0.2 to 0.2?
+
+##Modeling
+
+#first remove variables that are not applicable for prediction of cnt
+bikeTrain<-bikeTrain %>% select(-instant,-dteday)
+
+#we can also remove variables where their information is captured by other variables
+#i.e., we do not need to include both weekday and workingday indicators.
+bikeTrain<-bikeTrain %>% select(-holiday,-weekday,-temp, -hum)
+
+view(bikeTrain)
+
+#now we are ready to fit the tree
+train_control<-trainControl(method="LOOCV")
+treeFit<-train(cnt ~ ., data=bikeTrain, method="rpart", 
+               preProcess=c("center","scale"),
+               trControl=train_control, 
+               tuneGrid=NULL)
+treeFit$results
+treeFit$bestTune
+
+#boosted tree model
+boostFit<-train(cnt ~ ., data=bikeTrain, method="gbm", 
+                preProcess=c("center","scale"),
+                trControl=train_control, 
+                tuneGrid=NULL) #why does it produce all those different iterations, with no apparent variation?
+
+boostFit$results
+boostFit$bestTune
+
+#predict values on test set and compare RMSEs
+treePred<-predict(treeFit,newdata=dplyr::select(bikeTest,-cnt))
+postResample(treePred,bikeTest$cnt)
+
+boostPred<-predict(boostFit,newdata=dplyr::select(bikeTest,-cnt))
+postResample(boostPred,bikeTest$cnt)
